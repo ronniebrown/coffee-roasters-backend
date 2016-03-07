7eb5b7a7c44564221d7ee12785587252d3ed2b0f
Ronnie Brown <ronnievoice@gmail.com>
2016-03-07 10:43:39 -0800

    first commit

---
+++ server/api/category/categoryRoutes.js
@@ -0,0 +1,19 @@
+var router = require('express').Router();
+var logger = require('../../util/logger');
+var controller = require('./categoryController');
+var auth = require('../../auth/auth');
+var checkUser = [auth.decodeToken(), auth.getFreshUser()];
+
+// lock down the right routes :)
+router.param('id', controller.params);
+
+router.route('/')
+  .get( controller.get)
+  .post(checkUser, controller.post)
+
+router.route('/:id')
+  .get(controller.getOne)
+  .put(checkUser, controller.put)
+  .delete(checkUser, controller.delete)
+
+module.exports = router;
